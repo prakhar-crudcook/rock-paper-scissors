655f5a9324d4b898ade9e517debce6fb11573481
prakharCrudcook <prakhar@crudcook.com>
2024-06-20 14:31:49 +0530

    Made some optimization to index.js

--- index.js
+++ index.js
@@ -9,21 +9,36 @@ let score = {
 
 const savedScore = JSON.parse(localStorage.getItem('score'));
 
-if(savedScore) score = savedScore;
-updateScore();
-
-function updateScore(){
+const updateScore = () => {
     document.querySelector('.score-display').innerHTML = `Wins: ${score.wins} | Losses: ${score.losses} | Ties: ${score.ties}`;
 }
 
-function updateResult(playerMoveImage, computerMoveImage, result){
+const updateLosses = () => {
+    score.losses++;
+    return "You Lose!";
+}
+
+const updateWins = () => {
+    score.wins++;
+    return "You Win!";
+}
+
+const updateTies = () => {
+    score.ties++;
+    return "It's a Tie!";
+}
+
+const updateResult = (playerMoveImage, computerMoveImage, result) => {
     localStorage.setItem('score', JSON.stringify(score));
     updateScore();
     document.querySelector('.moves-display').innerHTML = `You <img src="${playerMoveImage}"> <img src="${computerMoveImage}"> Computer`;
     document.querySelector('.result-display').innerHTML = `${result}`;
 }
 
-function makeMove(playerMove){
+if(savedScore) score = savedScore;
+updateScore();
+
+const makeMove= (playerMove) => {
 
     let computerMove;
     let computerMoveImage = "";
@@ -41,44 +56,129 @@ function makeMove(playerMove){
 
     computerMoveImage = `./assets/${computerMove}.png`;
 
-    if(playerMove === computerMove){
-        score.ties++;
-        result = "It's a Tie!";
-        updateResult(playerMoveImage, computerMoveImage, result);
-    } else if(playerMove === 'rock'){
-        if(computerMove === 'paper'){
-            score.losses++;
-            result = "You Lose.";
-            updateResult(playerMoveImage, computerMoveImage, result);
-        } else {
-            score.wins++;
-            result = "You Win!";
-            updateResult(playerMoveImage, computerMoveImage, result);
-        }
-    } else if(playerMove === 'paper'){
-        if(computerMove === 'rock'){
-            score.wins++;
-            result = "You Win!";
-            updateResult(playerMoveImage, computerMoveImage, result);
-        } else {
-            score.losses++;
-            result = "You Lose.";
-            updateResult(playerMoveImage, computerMoveImage, result);
-        }
-    } else{
-        if(computerMove === 'rock'){
-            score.losses++;
-            result = "You Lose.";
-            updateResult(playerMoveImage, computerMoveImage, result);
-        } else {
-            score.wins++;
-            result = "You Win!";
-            updateResult(playerMoveImage, computerMoveImage, result);
-        }
+    // switch(playerMove){
+    //     case 'rock':
+    //         switch(computerMove) {
+    //             case "rock":
+    //                 result = updateTies();
+    //                 break;
+
+    //             case "paper":
+    //                 result = updateLosses();
+    //                 break;
+
+    //             case "scissors":
+    //                 result = updateWins();
+    //                 break;
+
+    //             default:
+    //                 console.log("Error!");
+    //         }
+
+    //     case "paper":
+    //         switch(computerMove) {
+    //             case "rock":
+    //                 result = updateWins();
+    //                 break;
+
+    //             case "paper":
+    //                 result = updateTies();
+    //                 break;
+
+    //             case "scissors":
+    //                 result = updateLosses();
+    //                 break;
+                
+    //             default:
+    //                 console.log("Error!");
+    //         }
+
+    //     case "scissors":
+    //         switch(computerMove) {
+    //             case "rock":
+    //                 result = updateLosses();
+    //                 break;
+
+    //             case "paper":
+    //                 result = updateWins();
+    //                 break;
+
+    //             case "scissors":
+    //                 result = updateTies();
+    //                 break;
+
+    //             default:
+    //                 console.log("Error!");
+    //         }
+
+    //     default:
+    //         console.log("Please make a valid move.");
+
+    // }
+
+
+    switch(playerMove){
+        case 'rock':
+            if(computerMove === 'rock') {
+                result = updateTies();
+            } else if(computerMove === 'paper') {
+                result = updateLosses();
+            } else {
+                result = updateWins();
+            }
+            break;
+
+        case "paper":
+            if(computerMove === 'rock') {
+                result = updateWins();
+            } else if(computerMove === 'paper') {
+                result = updateTies();
+            } else {
+                result = updateLosses();
+            }
+            break;
+
+        case "scissors":
+            if(computerMove === 'rock') {
+                result = updateLosses();
+            } else if(computerMove === 'paper') {
+                result = updateWins();
+            } else {
+                result = updateTies();
+            }
+            break;
+
+        default:
+            console.log("Please make a valid move.");
     }
+
+
+    // if(playerMove === computerMove){
+    //     result = updateTies();
+    // } else if(playerMove === 'rock'){
+    //     if(computerMove === 'paper'){
+    //         result = updateLosses();
+    //     } else {
+    //         result = updateWins();
+    //     }
+    // } else if(playerMove === 'paper'){
+    //     if(computerMove === 'rock'){
+    //         result = updateWins();
+    //     } else {
+    //         result = updateLosses();
+    //     }
+    // } else{
+    //     if(computerMove === 'rock'){
+    //         result = updateLosses();
+    //     } else {
+    //         result = updateWins();
+    //     }
+    // }
+
+    updateResult(playerMoveImage, computerMoveImage, result);
 }
 
-function resetScore() {
+const resetScore = () => {
     score.wins = 0;
     score.losses = 0;
     score.ties = 0;
